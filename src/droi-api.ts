10aef6de493ca04627c518860e089ebe4566d330
Skyer <skyer.tai@droi.com.tw>
2017-10-24 17:09:13 +0800

    * update gitignore * add xmlhttprequest for node * droi-api (WIP) * touch droi-error * touch index.ts

--- src/droi-api.ts
+++ src/droi-api.ts
@@ -1,3 +1,31 @@
 // Droi rest-apis wrapper
 //
 // 2 implementation - HTTPS(RestAPI) and DroiSecure HTTP for each api
+
+import DroiError from "./droi-error"
+
+namespace RemoteServiceHelper {
+    export interface HeaderMap {
+        [key: string]: string;
+    }
+
+    export class TokenHolder {
+        static AUTO_TOKEN = new TokenHolder("", true);
+
+        private token: string;
+        private isAuto: boolean;
+
+        private constructor(token: string, isAuto: boolean) {
+            this.token = token;
+            this.isAuto = isAuto;
+        }
+
+        private make(token: string): TokenHolder {
+            return new TokenHolder(token, false);
+        }
+    }
+
+    export function callServer(urlPath: string, method: string, input: string, headers:HeaderMap, tokenHolder: TokenHolder, error: DroiError): string {
+        return "";
+    }
+}
