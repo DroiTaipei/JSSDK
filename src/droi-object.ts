da1afa5f0728b5c0d68a1c61db8caa8b238137ee
Jerry Chan <jerryctt@Jerryde-Mini.dev.droitp.localnet>
2017-10-30 17:03:15 +0800

    Add serialize/deserialize

--- src/droi-object.ts
+++ src/droi-object.ts
@@ -25,6 +25,7 @@ class DroiObject {
 
         // createdTime, modifiedTime
         let currentDate = new Date();
+        this.properties[ DroiConstant.DROI_KEY_JSON_CLASSNAME ] = tableName;
         this.properties[ DroiConstant.DROI_KEY_JSON_OBJECTID ] = Guid.newGuid();
         this.properties[ DroiConstant.DROI_KEY_JSON_CREATION_TIME ] = currentDate.toISOString();
         this.properties[ DroiConstant.DROI_KEY_JSON_MODIFIED_TIME ] = currentDate.toISOString();
@@ -54,32 +55,120 @@ class DroiObject {
         if ( keyName == DroiConstant.DROI_KEY_JSON_OBJECTID ||
             keyName == DroiConstant.DROI_KEY_JSON_CREATION_TIME ||
             keyName == DroiConstant.DROI_KEY_JSON_MODIFIED_TIME ||
-            keyName == DroiConstant.DROI_KEY_JSON_TABLE_NAME )
+            keyName == DroiConstant.DROI_KEY_JSON_CLASSNAME )
             return;
+
+        // TODO: Normally, we should check the datatype of all variables
+        let val = null;
+        if ( value instanceof Date ) {
+            val = value.toISOString();
+        } else if (value instanceof DroiObject ) {
+            val = value;
+        } else if ( value instanceof Array ) {
+            var r = [];
+            for ( let i of value ) {
+                r.push( i );
+            }
+            val = r;
+        } else if ( typeof value === 'object' ) {   // 
+            let dict = {};
+            for ( let item in value ) {
+                dict[item] = value[item];
+            }
+            val = dict;
+        } else if ( typeof value === 'string' || typeof value === 'number' )
+            val = value;
+
+        // Return the value
+        if ( val != null ) {
+            this.properties[ keyName ] = val;
+        }
     }
 
     getValue( keyName : string ) : any {
         if ( keyName == DroiConstant.DROI_KEY_JSON_OBJECTID ||
             keyName == DroiConstant.DROI_KEY_JSON_CREATION_TIME ||
             keyName == DroiConstant.DROI_KEY_JSON_MODIFIED_TIME ||
-            keyName == DroiConstant.DROI_KEY_JSON_TABLE_NAME )
+            keyName == DroiConstant.DROI_KEY_JSON_CLASSNAME ||
+            this.properties[keyName] === undefined )
             return null;
 
+        return this.properties[keyName];
     }
 
     toJson() : string {
-        let clone = DroiObject.deepCopy( this.properties );
+        return this.toJSON();
+    }
 
+    // For JSON.stringify function
+    toJSON() : string {
+        let clone = DroiObject.exportProperties( this.properties );
+        
         // 
-        return JSON.stringify( clone );
+        return JSON.stringify( clone );        
+    }
+
+    static fromJson( jobj : any ) : any {
+        let res = null;
+
+        // Parse first..
+        if ( typeof jobj === 'string' ) {
+            try{
+                jobj = JSON.parse( jobj );
+            } catch (e) {
+                return null;
+            }
+        }
+
+        if ( jobj instanceof Array ) {
+            let array = new Array();
+            for ( let item of jobj ) {
+                let r = DroiObject.fromJson( item );
+                if ( r == null ) {
+                    if ( typeof item === 'string' || typeof item === 'number' || typeof item === 'object' )
+                        array.push( item );
+                } else
+                    array.push( r );
+            }
+            res = array;
+        } else if ( typeof jobj === 'object' ) {    // Dictionary
+
+            // Check whether this object is DroiObject ??
+            if ( jobj[DroiConstant.DROI_KEY_JSON_CLASSNAME] !== undefined &&
+                jobj[DroiConstant.DROI_KEY_JSON_OBJECTID] !== undefined &&
+                jobj[DroiConstant.DROI_KEY_JSON_CREATION_TIME] !== undefined &&
+                jobj[DroiConstant.DROI_KEY_JSON_MODIFIED_TIME] !== undefined ) {
+                let tableName = jobj[DroiConstant.DROI_KEY_JSON_CLASSNAME];
+                // TODO: Check tableName
+                let r = DroiObject.createObject( tableName );
+
+                // Copy the key-Value into object
+                for ( let keyName in jobj ) {
+                    let v = jobj[keyName];
+                    let o = DroiObject.fromJson( v );
+                    if ( o != null ) { 
+                        r.properties[keyName] = o;
+                    } else if ( typeof v === 'string' || typeof v === 'number' || typeof v === 'object' ) {
+                        r.properties[keyName] = v;
+                    }
+                }
+
+                res = r;
+            }
+        } 
+
+        return res;
     }
 
     //
-    private static deepCopy(obj) : any {
-        var copy;
+    private static exportProperties(obj) : any {
+        let copy;
     
         // Handle the 3 simple types, and null or undefined
-        if (null == obj || "object" != typeof obj) return obj;
+        if (null == obj || "object" !== typeof obj) return obj;
+
+        if ( obj instanceof DroiObject )
+            return DroiObject.exportProperties( obj.properties );
     
         // Handle Date
         if (obj instanceof Date) {
@@ -91,8 +180,8 @@ class DroiObject {
         // Handle Array
         if (obj instanceof Array) {
             copy = [];
-            for (var i = 0, len = obj.length; i < len; i++) {
-                copy[i] = DroiObject.deepCopy(obj[i]);
+            for (let i = 0, len = obj.length; i < len; i++) {
+                copy[i] = DroiObject.exportProperties(obj[i]);
             }
             return copy;
         }
@@ -101,7 +190,7 @@ class DroiObject {
         if (obj instanceof Object) {
             copy = {};
             for (var attr in obj) {
-                if (obj.hasOwnProperty(attr)) copy[attr] = DroiObject.deepCopy(obj[attr]);
+                if (obj.hasOwnProperty(attr)) copy[attr] = DroiObject.exportProperties(obj[attr]);
             }
             return copy;
         }
--- src/test/main.ts
+++ src/test/main.ts
@@ -1,6 +1,16 @@
 
 import * as DroiBaaS from '../index';
 
-var test = DroiBaaS.DroiObject.createObject("TEST");
-
-console.log( test.toJson() );
+let test = DroiBaaS.DroiObject.createObject("TEST");
+let test1 = DroiBaaS.DroiObject.createObject("TEST1");
+test.setValue( "test", "test value" );
+test.setValue( "T", "T value" );
+test.setValue( "NUM", 12345 );
+test.setValue( "array", [8, 2, 3, 4]);
+test.setValue( "dict", { "12":12, "24":24 });
+test.setValue( "test1", test1 );
+console.log( 'test is ' + test.toJson() );
+let s = test.toJson();
+console.log("The s is " + s );
+let res = DroiBaaS.DroiObject.fromJson( s );
+console.log( 'res is ' + res.toJson() ); 
